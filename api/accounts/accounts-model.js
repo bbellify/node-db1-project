e3a722a4d2124cb6f7fb88e417019b71dd02da80
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-06 18:14:49 -0800

    wip

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,25 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+const getAll =  async () => {
+  // select * from accounts
+  const rows = await db('accounts')
+    .select('id', 'name', 'budget')
+    return rows
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async id => {
+  // select * from accounts where id=12
+  const record = await db('accounts')
+    .where('id', id)
+    return record
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  // insert into accounts (name, budget) values ('account-14', '5000');
+  const [newid] = await db('accounts').insert(account)
+  const newAccount = await getById(newid)
+  return newAccount
+
 }
 
 const updateById = (id, account) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,15 +1,34 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
+
+const { 
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId,
+} = require('./accounts-middleware')
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.getAll()
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(next)
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.getById(req.params.id)
+    .then( rec => {
+      res.json(rec)
+    })
+    .catch(next)
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.create(req.body)
+    .then(rec => {
+      res.json(rec)
+    })
+    .catch(next)
 })
 
 router.put('/:id', (req, res, next) => {
@@ -17,11 +36,14 @@ router.put('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 400).json({
+    message: `${err.message}`,
+    stack: err.stack
+  })
 })
 
 module.exports = router;
